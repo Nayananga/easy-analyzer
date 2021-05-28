ed778ccf9fc92a8e0e91b924010859a45256c7e1
nayananga <nayanangamuhandiram@gmail.com>
2021-05-28 18:13:05 +0530

    Code reformatted

--- easy_ana.py
+++ easy_ana.py
@@ -11,6 +11,24 @@ async def serve(q: Q):
         plot_data = read_data(q.args.file_upload)
         q.client.data = plot_data
 
+    elif 'draw' in q.args:
+        plot_data = q.client.data
+        plot_data = generate_plot_data(plot_data, q.args.x_axis, q.args.y_axis)
+
+        v = q.page.add('content', ui.plot_card(
+            box=ui.boxes('content'),
+            title=q.args.title,
+            data=data('group x y'),
+            plot=ui.plot([ui.mark(type='line', x='=x', x_scale='linear', y='=y', y_scale='linear', color='=group',
+                                  x_title=q.args.x_axis)])
+        ))
+
+        v.data = plot_data
+
+    if q.client.initialized:
+
+        plot_data = q.client.data
+
         q.page['meta_1'] = ui.meta_card(box='', layouts=[
             ui.layout(
                 # If the viewport width >= 0:
@@ -19,68 +37,13 @@ async def serve(q: Q):
                     # 80px high header
                     ui.zone('header', size='80px'),
                     # Use remaining space for content
-                    ui.zone('sidebar')
-                ]
-            ),
-            ui.layout(
-                # If the viewport width >= 768:
-                breakpoint='m',
-                zones=[
-                    # 80px high header
-                    ui.zone('header', size='80px'),
-                    # Use remaining space for body
-                    ui.zone('body', direction=ui.ZoneDirection.ROW, zones=[
-                        # 250px wide sidebar
-                        ui.zone('sidebar', size='250px'),
-                    ]),
-                    ui.zone('footer'),
-                ]
-            ),
-            ui.layout(
-                # If the viewport width >= 1200:
-                breakpoint='xl',
-                width='1200px',
-                zones=[
-                    # 80px high header
-                    ui.zone('header', size='80px'),
-                    # Use remaining space for body
-                    ui.zone('body', direction=ui.ZoneDirection.ROW, zones=[
-                        # 300px wide sidebar
+                    ui.zone('body', direction=ui.ZoneDirection.COLUMN, zones=[
+                        # Use remaining space for content
+                        ui.zone('content'),
+                        # wide sidebar
                         ui.zone('sidebar', size='300px'),
                     ]),
                     ui.zone('footer'),
-                ]
-            )
-        ])
-
-        q.page['controls'] = ui.form_card(box=ui.boxes('sidebar'), items=[
-            ui.message_bar(type='success', text=f"Great! your file uploaded successfully!"),
-            ui.separator(),
-            ui.dropdown(name='x_axis', label='Pick your X axis', choices=[
-                ui.choice(name=x, label=x) for x in plot_data
-            ]),
-            ui.separator(),
-            ui.dropdown(name='y_axis', label='Pick your Y axis', values=[], choices=[
-                ui.choice(name=y, label=y) for y in plot_data
-            ]),
-            ui.separator(),
-            ui.textbox(name='title', label='Enter your plot title'),
-            ui.separator(),
-            ui.button(name='draw', label='Draw', primary=True),
-        ])
-
-    if 'draw' in q.args:
-        plot_data = q.client.data
-
-        q.page['meta_2'] = ui.meta_card(box='', layouts=[
-            ui.layout(
-                # If the viewport width >= 0:
-                breakpoint='xs',
-                zones=[
-                    # 80px high header
-                    ui.zone('header', size='80px'),
-                    # Use remaining space for content
-                    ui.zone('content')
                 ]
             ),
             ui.layout(
@@ -118,15 +81,6 @@ async def serve(q: Q):
             )
         ])
 
-        v = q.page.add('content', ui.plot_card(
-            box=ui.boxes('content'),
-            title=q.args.title,
-            data=data('group x y'),
-            plot=ui.plot([ui.mark(type='line', x='=x', x_scale='linear', y='=y', y_scale='linear', color='=group', x_title=q.args.x_axis)])
-        ))
-
-        v.data = generate_plot_data(plot_data, q.args.x_axis, q.args.y_axis)
-
         q.page['controls'] = ui.form_card(box=ui.boxes('sidebar'), items=[
             ui.message_bar(type='success', text=f"Great! your file uploaded successfully!"),
             ui.separator(),
@@ -143,16 +97,17 @@ async def serve(q: Q):
             ui.button(name='draw', label='Draw', primary=True),
         ])
 
-    if not q.client.initialized:
-        q.page['meta_3'] = ui.meta_card(box='', layouts=[
+    else:
+        q.page['meta_2'] = ui.meta_card(box='', layouts=[
             ui.layout(
                 # If the viewport width >= 0:
                 breakpoint='xs',
                 zones=[
                     # 80px high header
                     ui.zone('header', size='80px'),
-                    # Use remaining space for content
-                    ui.zone('content')
+                    # Use remaining space for content and footer
+                    ui.zone('content'),
+                    ui.zone('footer'),
                 ]
             ),
             ui.layout(
